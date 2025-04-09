035b73f7130a507f0bb853b03fd1e1cd30197361
jungseobum <65856247+jungseobum@users.noreply.github.com>
2025-04-09 19:09:50 +0900

    Add files via upload

---
+++ arrange.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+for file in ./files/*; do
+  if [ -f "$file" ]; then
+    filename=$(basename "$file")
+    first_char="${filename:0:1}"
+
+    case "$first_char" in
+      A|a) folder="a" ;;
+      B|b) folder="b" ;;
+      C|c) folder="c" ;;
+      D|d) folder="d" ;;
+      E|e) folder="e" ;;
+      F|f) folder="f" ;;
+      G|g) folder="g" ;;
+      H|h) folder="h" ;;
+      I|i) folder="i" ;;
+      J|j) folder="j" ;;
+      K|k) folder="k" ;;
+      L|l) folder="l" ;;
+      M|m) folder="m" ;;
+      N|n) folder="n" ;;
+      O|o) folder="o" ;;
+      P|p) folder="p" ;;
+      Q|q) folder="q" ;;
+      R|r) folder="r" ;;
+      S|s) folder="s" ;;
+      T|t) folder="t" ;;
+      U|u) folder="u" ;;
+      V|v) folder="v" ;;
+      W|w) folder="w" ;;
+      X|x) folder="x" ;;
+      Y|y) folder="y" ;;
+      Z|z) folder="z" ;;
+      *) continue ;;
+    esac
+      mv "$file" "$folder/"
+  fi
+done
+
